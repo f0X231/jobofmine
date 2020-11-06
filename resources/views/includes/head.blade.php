a4d24aac7aacae158382637cce4ee1d3a661796f
zhaii <guChai@gmail.com>
2020-11-06 08:39:16 +0700

    view blade * mutilingual

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,30 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>{{$seo['title']}}</title>
+<meta name="robots" content="noindex, nofollow">
+<!--meta name="robots" content="index, follow"-->
+
+
+<!-- Meta tags SEO -->
+<meta name="description" content="{{$seo['description']}}"> <!-- Maximum 300 characters -->
+<meta name="keywords" content="{{$seo['keyword']}}">
+<!-- Open Graph data -->
+<meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="When Great Minds Don’t Think Alike" />
+<meta property="og:description" content="How much does culture influence creative thinking?" />
+<meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
+<!-- Twitter Card data -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@publisher_handle">
+<meta name="twitter:title" content="Page Title">
+<meta name="twitter:description" content="Page description less than 200 characters">
+<meta name="twitter:creator" content="@author_handle">
+<meta name="twitter:image" content="http://www.example.com/image.jpg">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
+    crossorigin="anonymous" />
