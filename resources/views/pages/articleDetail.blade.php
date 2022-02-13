6d4e3e4f06f76181677e1f85abe6433b161247dc
chai <apichai.s@trendvg3.com>
2022-02-13 14:11:15 +0700

    update article detail empty

--- resources/views/pages/articleDetail.blade.php
+++ resources/views/pages/articleDetail.blade.php
@@ -36,26 +36,28 @@
       </div>
     </div>
 
-    <div class="row">
-        <div class="col-12 article__txt fontSize15rem">{!! $data['detail']['th'] !!}</div>
-        <div class="col-12 fontSize125rem">
-          <br />
-          <br />
-          <p>{{ __('article.txt_credit') }} : {{$data['credit']}}</p>
-          <p>{{ __('article.txt_footer') }}</p>
-          <p>{{ __('article.txt_connect_txt') }} &middot; {!! __('article.txt_phone') !!} &middot; {!! __('article.txt_line') !!}</p>
-        </div>
-        <div class="col-12 fontSize125rem text-center">
-          <br />
-          <br />
-          <a href="{{ URL::to('articles') }}">
-            <b>
-              <img src="{{ asset('images/back.svg') }}" class="icoNavbar" /> {{ __('article.txt_goback') }}
-            </b>
-          </a>
-          <br />
-        </div>
-    </div>
+    @if(!empty($data['detail']['th']))
+      <div class="row">
+          <div class="col-12 article__txt fontSize15rem">{!! $data['detail']['th'] !!}</div>
+          <div class="col-12 fontSize125rem">
+            <br />
+            <br />
+            <p>{{ __('article.txt_credit') }} : {{$data['credit']}}</p>
+            <p>{{ __('article.txt_footer') }}</p>
+            <p>{{ __('article.txt_connect_txt') }} &middot; {!! __('article.txt_phone') !!} &middot; {!! __('article.txt_line') !!}</p>
+          </div>
+          <div class="col-12 fontSize125rem text-center">
+            <br />
+            <br />
+            <a href="{{ URL::to('articles') }}">
+              <b>
+                <img src="{{ asset('images/back.svg') }}" class="icoNavbar" /> {{ __('article.txt_goback') }}
+              </b>
+            </a>
+            <br />
+          </div>
+      </div>
+    @endif
 </div>
 
 @stop
