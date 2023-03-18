cd9bcaa60146a5301120d92ab76ad2d31c4c0f1d
zhaii <apichai.s@thairathonline.co.th>
2023-03-18 20:37:21 +0700

    add gtm [G-D3J2E51CY4]

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -45,6 +45,14 @@
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PR8PF2T');
 </script>
 <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D3J2E51CY4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-D3J2E51CY4');
+</script>
 
 <!-- Cookie Consent -->
 <script src="/js/glowCookies.js"></script>
@@ -71,6 +79,6 @@
         //rejectBtnColor: 'blue',
         //manageColor: 'white',
         //manageBackground: 'blue',
-        //manageText: 'cookies text'
+        manageText: 'cookies text'
     });
 </script>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,6 +7,7 @@
 <body>
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PR8PF2T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-D3J2E51CY4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
     @include('includes.header')
