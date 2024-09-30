e3bff3ae8a4cbca6296ffec5a7bd5ceed6ed1b28
zhaii <apichai.s@thairathonline.co.th>
2024-10-01 00:22:45 +0700

    new gtm

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,9 @@
 
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PR8PF2T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-D3J2E51CY4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    {{-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PR8PF2T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> --}}
+    {{-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-D3J2E51CY4" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> --}}
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PX36KKB" height="0" width="0" style="display:none;visibility:hidden"></iframe>
     <!-- End Google Tag Manager (noscript) -->
 
     @include('includes.header')
